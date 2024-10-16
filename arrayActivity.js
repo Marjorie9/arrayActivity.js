5a4cb6d5ecc0d6a42a57234c4f9a924b0a9b4b93
Marjorie9 <marjoriekatedeguzman@gmail.com>
2024-10-16 18:09:39 +0800

    Add files via upload

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+const favSingers = ["Adele", "Taylor Swift", "Glenn Frey"];
+
+console.log(favSingers[1]);
+
+const favNumbers = [17, 8, 14, 19];
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
